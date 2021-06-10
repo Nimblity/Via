125b5020d05df5ebcd3411ea917ab9c5303d114b
Nimblity <akishore@nimblitysolutions.com>
2021-06-10 10:32:42 +0530

    D:10-06-21: Creating SHC Generator project

---
+++ SHC-ReportGenerator/src/com/nmb/reportgenerator/ReportGenerator.java
@@ -0,0 +1,8 @@
+package com.nmb.reportgenerator;
+
+public class ReportGenerator {
+	public static void main(String[] args) {
+		System.out.println("Hello and welcome to report generator");
+	}
+
+}
